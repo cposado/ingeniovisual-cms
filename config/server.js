2112f43c981fe2b29a889d9a5f4bf6dfdc56a646
Claudio Posado <claudio@overprint.com.ar>
2021-01-26 14:56:06 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a2984472ad06098041a1664c07cb49a6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '20036055-0cd3-4a71-a959-3dcae80d5235'
+};
